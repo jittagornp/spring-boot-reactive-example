a88419405a27a7c878439aa158f44da9fe3b4ce5
jittagornp <jittagornp@gmail.com>
2019-06-24 22:43:10 +0700

    add filter module

---
+++ spring-boot-webflux-filter/src/main/java/com/pamarin/learning/webflux/filter/controller/HomeController.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright 2017-2019 Pamarin.com
+ */
+package com.pamarin.learning.webflux.filter.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+/**
+ *
+ * @author jitta
+ */
+@RestController
+public class HomeController {
+
+
+    @GetMapping({"", "/"})
+    public Mono<String> hello() {
+       return Mono.just("Hello world.");
+    }
+
+}
