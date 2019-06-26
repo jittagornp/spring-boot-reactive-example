863ee51d1337fcc5c2aea8b6ca620e381cf80521
jittagornp <jittagornp@gmail.com>
2019-06-26 18:06:51 +0700

    move to config package

---
+++ spring-boot-webflux-scheduling/src/main/java/com/pamarin/learning/webflux/config/SchedulingConfig.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright 2017-2019 Pamarin.com
+ */
+package com.pamarin.learning.webflux.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ *
+ * @author jitta
+ */
+@Configuration
+@EnableScheduling
+public class SchedulingConfig {
+    
+}
