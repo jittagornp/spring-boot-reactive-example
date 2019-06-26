29723b517ea5f5a2f062ec3f7092a1065dbd0aac
jittagornp <jittagornp@gmail.com>
2019-06-26 12:09:55 +0700

    add module logging

--- spring-boot-webflux-logging/src/main/java/com/pamarin/learning/webflux/controller/HomeController.java
+++ spring-boot-webflux-logging/src/main/java/com/pamarin/learning/webflux/controller/HomeController.java
@@ -3,6 +3,7 @@
  */
 package com.pamarin.learning.webflux.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
@@ -11,11 +12,13 @@ import reactor.core.publisher.Mono;
  *
  * @author jitta
  */
+@Slf4j
 @RestController
 public class HomeController {
 
     @GetMapping({"", "/"})
     public Mono<String> hello() {
+        log.debug("call hello method");
         return Mono.just("Hello world.");
     }
 }
