1881786bc16857f6897e998880405af45675b5f6
jittagornp <jittagornp@gmail.com>
2019-07-05 16:48:07 +0700

    add logout

--- spring-boot-webflux-custom-login-page/src/main/java/com/pamarin/learning/webflux/config/SecurityConfig.java
+++ spring-boot-webflux-custom-login-page/src/main/java/com/pamarin/learning/webflux/config/SecurityConfig.java
@@ -6,6 +6,7 @@ package com.pamarin.learning.webflux.config;
 import java.util.Collections;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
@@ -13,6 +14,7 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.util.matcher.ServerWebExchangeMatchers;
 import reactor.core.publisher.Mono;
 
 /**
@@ -33,6 +35,10 @@ public class SecurityConfig {
                 .and()
                 .formLogin()
                 .loginPage("/login")
+                .and()
+                .logout()
+                .logoutUrl("/logout")
+                .requiresLogout(ServerWebExchangeMatchers.pathMatchers(HttpMethod.GET, "/logout"))
                 .and()
                 .build();
     }
