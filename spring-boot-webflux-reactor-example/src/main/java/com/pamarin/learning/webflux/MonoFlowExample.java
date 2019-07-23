aaf2ee5583f0d4beabec907749c9a7e1fa671082
jittagornp <jittagornp@gmail.com>
2019-07-23 19:55:08 +0700

    add FluxFlow and MonoFlow example

--- spring-boot-webflux-reactor-example/src/main/java/com/pamarin/learning/webflux/MonoFlowExample.java
+++ spring-boot-webflux-reactor-example/src/main/java/com/pamarin/learning/webflux/MonoFlowExample.java
@@ -4,28 +4,30 @@
 package com.pamarin.learning.webflux;
 
 import lombok.extern.slf4j.Slf4j;
-import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 /**
  *
  * @author jitta
  */
 @Slf4j
-public class FluxFlowExample {
+public class MonoFlowExample {
 
     public static void main(String[] args) {
-        Flux.just("A", "B", "C")
+        Mono.just("A")
                 .doFirst(() -> log.debug("doFirst..."))
                 .doOnRequest(value -> log.debug("doOnRequest... {}", value))
                 .doOnEach(signal -> log.debug("doOnEach... {} : value => {}", signal.getType().name(), signal.get()))
                 .doOnNext(value -> log.debug("doOnNext... {}", value))
                 .doOnCancel(() -> log.debug("doOnCacel..."))
                 .doOnError(e -> log.debug("doOnError... {}", e.getMessage()))
-                .doOnComplete(() -> log.debug("doOnComplete..."))
+                .doOnSuccess(value -> log.debug("doOnSuccess... {}", value))
+                .doOnSuccessOrError((value, e) -> log.debug("doOnSuccessOrError... {} or {}", value, (e == null ? null : e.getMessage())))
+                .doAfterSuccessOrError((value, e) -> log.debug("doAfterSuccessOrError... {} or {}", value, (e == null ? null : e.getMessage())))
                 .doAfterTerminate(() -> log.debug("doAfterTerminate..."))
                 .doOnTerminate(() -> log.debug("doOnTerminate..."))
                 .doOnSubscribe(subscription -> {
-                    long id = 123456890;
+                    long id = 1234567890;
                     subscription.request(id);
                     log.debug("doOnSubscribe... {}", id);
                 })
