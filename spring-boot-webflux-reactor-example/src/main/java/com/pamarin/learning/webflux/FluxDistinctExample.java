3e65238bca1def2883e6ca18134edab1960467df
jittagornp <jittagornp@gmail.com>
2019-07-23 16:14:12 +0700

    add Mono.distinct example

---
+++ spring-boot-webflux-reactor-example/src/main/java/com/pamarin/learning/webflux/FluxDistinctExample.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright 2017-2019 Pamarin.com
+ */
+package com.pamarin.learning.webflux;
+
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Flux;
+
+/**
+ *
+ * @author jitta
+ */
+@Slf4j
+public class FluxDistinctExample {
+    
+    public static void main(String[] args) {
+        Flux.just("A", "B", "C", "A", "A", "B", "D")
+                .distinct()
+                .doOnNext(message -> {
+                    log.debug("message => {}", message);
+                })
+                .subscribe();
+    }
+    
+}
