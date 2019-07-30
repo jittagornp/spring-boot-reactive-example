9e459a79d6676cb9b06b241bcd4e969557b98ba0
jittagornp <jittagornp@gmail.com>
2019-07-30 10:46:41 +0700

    add Flux.error example

--- spring-boot-webflux-reactor-example/src/main/java/com/pamarin/learning/webflux/FluxErrorExample.java
+++ spring-boot-webflux-reactor-example/src/main/java/com/pamarin/learning/webflux/FluxErrorExample.java
@@ -4,18 +4,18 @@
 package com.pamarin.learning.webflux;
 
 import lombok.extern.slf4j.Slf4j;
-import reactor.core.publisher.Mono;
+import reactor.core.publisher.Flux;
 
 /**
  *
  * @author jitta
  */
 @Slf4j
-public class MonoErrorExample {
+public class FluxErrorExample {
     
     public static void main(String[] args) {
-        Mono.justOrEmpty(null)
-                .switchIfEmpty(Mono.error(new RuntimeException("Not found data")))
+        Flux.empty()
+                .switchIfEmpty(Flux.error(new RuntimeException("Not found data")))
                 .doOnNext(message -> {
                     log.debug("message => {}", message);
                 })
