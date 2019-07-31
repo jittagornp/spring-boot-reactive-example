d2e25c466ad8068c007c50022412d2f28adf4520
jittagornp <jittagornp@gmail.com>
2019-07-31 12:12:32 +0700

    add MonoThenWarning example

---
+++ spring-boot-webflux-reactor-example/src/main/java/com/pamarin/learning/webflux/MonoThenWarningExample.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2017-2019 Pamarin.com
+ */
+package com.pamarin.learning.webflux;
+
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Mono;
+
+/**
+ *
+ * @author jitta
+ */
+@Slf4j
+public class MonoThenWarningExample {
+
+    public static void main(String[] args) {
+        Mono<String> task1 = Mono.fromCallable(() -> {
+            log.debug("do task 1");
+            return "task 1";
+        });
+        Mono<String> task2 = Mono.fromCallable(() -> {
+            log.debug("do task 2");
+            return "task 2";
+        });
+        Mono<String> task3 = Mono.defer(() -> {
+            log.debug("do task 3");
+            return Mono.error(new RuntimeException("something error"));
+        });
+        Mono<String> task4 = Mono.fromCallable(() -> {
+            log.debug("do task 4");
+            return "task 4";
+        });
+        Mono<String> task5 = Mono.fromCallable(() -> {
+            log.debug("do task 5");
+            return "task 5";
+        });
+
+        task1
+                .doOnNext(value -> log.debug("value => {}", value))
+                .then(task2)
+                .doOnNext(value -> log.debug("value => {}", value))
+                .then(task3)
+                .doOnNext(value -> log.debug("value => {}", value))
+                .then(task4)
+                .doOnNext(value -> log.debug("value => {}", value))
+                .then(task5)
+                .subscribe();
+    }
+
+}
