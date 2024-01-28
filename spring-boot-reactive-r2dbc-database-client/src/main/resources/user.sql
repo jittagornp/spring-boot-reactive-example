fe071c8729be9e6342d8876cbcf04c995d96fc51
Jittagorn Pitakmetagoon <jittagornp@gmail.com>
2024-01-28 21:36:37 +0700

    refactor: Remove example spring-boot-reactive-r2dbc-database-client

--- spring-boot-reactive-r2dbc-database-client/src/main/resources/user.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE "app"."user" (
-    "id" UUID NOT NULL,
-    "username" varchar(50) NOT NULL,
-    "first_name" varchar(50) NOT NULL,
-    "last_name" varchar(50) NOT NULL,
-    PRIMARY KEY ("id")
-);
